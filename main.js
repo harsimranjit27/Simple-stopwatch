65f6573566433a30437edfd0b9ce7005bd63a688
harsimranjit27 <harsimran2731@gmail.com>
2021-08-22 00:57:27 +0530

    Added pause functionality to stopwatch

--- main.js
+++ main.js
@@ -2,66 +2,62 @@ let minutes = document.querySelector("#minutes");
 let seconds = document.querySelector("#seconds");
 let start_btn = document.querySelector(".start");
 let stop_btn = document.querySelector(".stop");
-let pause_play_btn = document.querySelector(".pause_play_btn");
+let pause_btn = document.querySelector(".pause");
 
 let secondCount = 0;
 let minuteCount = 0;
 let startIntID;
-// let clearTmOutID;
-// let alerted = false;
+let count = 0;
 
 start_btn.addEventListener("click",startWatch);
 stop_btn.addEventListener("click",stopWatch);
-pause_play_btn.addEventListener("click",pausePlayWatch);
+pause_btn.addEventListener("click",pauseWatch);
 
 function startWatch(){
-
-    startIntID = setInterval(function(){
-
-        secondCount++;
-        if (secondCount < 10) {
-            seconds.innerText = "0"+secondCount;
-        }
-        else if(secondCount < 60){
-            seconds.innerText = secondCount;
-        }
-        
-        else {
-            
-            secondCount = 0;
-            seconds.innerText = "0"+secondCount;
-            minuteCount++;
-
-            if (minuteCount < 10) {
-                minutes.innerText = "0"+minuteCount;
-            }
-            else{
-                minutes.innerText = minuteCount;
-            }
-            
-            if (minuteCount == 60) {
-                clearInterval(startIntID);
-                minutes.innerText = 59;
-                seconds.innerText = 59;
-            }
-            
-        }
-    },1000);
-
-    // clearTmOutID = setTimeout(function() {
-    //     alerted = true;
-    //     alert(minuteCount + " minute(s) over.")
-    // },60000);
-
-    // if (alerted) {
-    //     clearTimeout(clearTmOutID);
-    // }
+    start_btn.disabled = true;
+    start_btn.innerText = "Start";
+    startIntID = setInterval(displayWatch,1000);
 }
-
+function displayWatch(){
+    secondCount++;
+    if (secondCount < 10) {
+        seconds.innerText = "0"+secondCount;
+    }
+    else if(secondCount < 60){
+        seconds.innerText = secondCount;
+    }  
+    else {        
+        secondCount = 0;
+        seconds.innerText = "0"+secondCount;
+        minuteCount++;
+        if (minuteCount < 10) {
+            minutes.innerText = "0"+minuteCount;
+        }
+        else{
+            minutes.innerText = minuteCount;
+        }       
+        if (minuteCount == 60) {
+            clearInterval(startIntID);
+            minutes.innerText = 59;
+            seconds.innerText = 59;
+            return;
+        }        
+    }
+}
 function stopWatch() {
+    clearInterval(startIntID);
     minutes.innerText = "00";
     seconds.innerText = "00";
     minuteCount = 0;
     secondCount = 0;
+    start_btn.disabled = false;
+    start_btn.innerText = "Start";
+}
+
+function pauseWatch() {
+    if (start_btn.disabled) {
+        start_btn.innerText = "Play";
+    }
+    start_btn.disabled = false;
     clearInterval(startIntID);
 }
